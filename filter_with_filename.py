a552d9567a0fb1e7d7520e318758eec1dd4a049a
Alexander Zaikin <ay.zaikin@gmail.com>
2021-11-21 20:37:58 +0500

    Рефакторинг по PEP8.

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -14,21 +14,21 @@ def discretize(number, step):
     return int(number//step) * step
 
 
-def apply_filter(image):
+def apply_filter(png_image):
     """Проходит по изображению и применяет фильтр к каждой клетке"""
     for x in range(0, width-filter_size+1, filter_size):
         for y in range(0, height-filter_size+1, filter_size):
-            filter = image[x:x+filter_size, y:y+filter_size]
-            average = np.average(filter)
+            filter_cell = png_image[x:x+filter_size, y:y+filter_size]
+            average = np.average(filter_cell)
             average = discretize(average, step_size)
-            filter.fill(average)
+            filter_cell.fill(average)
 
 
-def crop(image):
+def crop(png_image):
     """Обрезает изображение под размер фильтра"""
     cropped_width = (width//filter_size) * filter_size
     cropped_height = (height//filter_size) * filter_size
-    return image[:cropped_width, :cropped_height]
+    return png_image[:cropped_width, :cropped_height]
 
 
 input_path = "img2.jpg"
